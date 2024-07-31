41e65dddcc237059bef1fdc816a460a8db2b405f
Julaaaa <77327732+Julaaaa@users.noreply.github.com>
2024-07-31 13:00:34 +0200

    Removed some code

--- main.ts
+++ main.ts
@@ -1,15 +1,3 @@
-player.onChat("move forward", function () {
-    agent.move(FORWARD, 1)
-})
-player.onChat("move right ", function () {
-    agent.move(RIGHT, 1)
-})
-player.onChat("move back", function () {
-    agent.move(BACK, 1)
-})
-player.onChat("move left ", function () {
-    agent.move(RIGHT, 1)
-})
 player.onChat("dance", function () {
     agent.move(FORWARD, 2)
     agent.move(RIGHT, 1)
