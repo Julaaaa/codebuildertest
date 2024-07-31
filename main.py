69b1c24fb73691832a70f8ce3910dc166e21054a
Julaaaa <77327732+Julaaaa@users.noreply.github.com>
2024-07-31 14:06:37 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-player.onChat("dance", function () {
+def on_on_chat():
     agent.move(FORWARD, 2)
     agent.move(RIGHT, 1)
     agent.move(LEFT, 1)
     agent.move(UP, 3)
-})
+player.on_chat("dance", on_on_chat)
+
+player.say("Hello!")
