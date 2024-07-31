d384bcf6cd147fc25bad96e25c385b3735294304
Julaaaa <77327732+Julaaaa@users.noreply.github.com>
2024-07-31 11:18:34 +0200

    Added simple commands for agent

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-player.onChat("run", function () {
-	
+player.onChat("move forward", function () {
+    agent.move(FORWARD, 1)
 })
+player.onChat("move right ", function () {
+    agent.move(RIGHT, 1)
+})
+player.onChat("move back", function () {
+    agent.move(BACK, 1)
+})
+player.onChat("move left ", function () {
+    agent.move(RIGHT, 1)
+})
+player.onChat("dance", function () {
+    agent.move(FORWARD, 2)
+    agent.move(RIGHT, 1)
+    agent.move(LEFT, 1)
+    agent.move(UP, 3)
+})
+player.say("Hello!")
